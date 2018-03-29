7438f8a7be33d0f0a5dd67346030e4f96dc521da
MattNelson42 <37908296+MattNelson42@users.noreply.github.com>
2018-03-29 10:59:28 -0500

    First Commit
    Testing first commit

---
+++ EmployeeDictionary/EmployeeDictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var employees = new Dictionary<int, Employee>();
+
+            employees.Add(101,
+                new Employee { ID = 101, FirstName = "Jesse", LastName = "Pinkman" });
+            employees.Add(102,
+                new Employee { ID = 102, FirstName = "George", LastName = "Washington" });
+            employees.Add(103,
+                new Employee { ID = 103, FirstName = "Mark", LastName = "Smith" });
+
+
+            Console.WriteLine(employees[101]);
+            Console.WriteLine(employees[102]);
+            Console.WriteLine(employees[103]);
+            Console.Read();
+        }
+    }
+}
